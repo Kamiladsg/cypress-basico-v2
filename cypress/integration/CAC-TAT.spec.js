902b375cafc1145d34180b62f59ac1d38311a949
Kamila <kamiladsg@gmail.com>
2023-08-18 18:19:41 +0100

    ok

--- cypress/integration/CAC-TAT.spec.js
+++ cypress/integration/CAC-TAT.spec.js
@@ -1,12 +1,16 @@
 /// <reference types="Cypress" />
 
+const { functions } = require("cypress/types/lodash")
+
 describe('Central de Atendimento ao Cliente TAT', function() {
-    it('verifica o título da aplicação', function() {
+    beforeEach(function() {
         cy.visit('./src/index.html')
+    })
+        
+    }
+    it('verifica o título da aplicação', function() {
+        cy.title().should('be.equal', 'Central de Atendimento ao Cliente TAT')
 
-        cy.title().should("be.equal",'Central de Atendimento ao cliente TAT')
-
-  
     })
   })
   
